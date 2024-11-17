d31b59153258aa49e7384123cb8ca8ce8b68bab3
LestterX <lgs.alves@hotmail.com>
2024-11-17 18:29:13 -0300

    feat: add cors to main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import corsOptions from './CorsOptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: corsOptions,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
